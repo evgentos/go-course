a95bc21f9b7788b30e9212f611802516424c8911
evgentos <evgentos2009@gmail.com>
2025-11-02 07:41:42 +0300

    Arithmetic

---
+++ basics/constants.go
@@ -0,0 +1,15 @@
+package basics
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const DAYS int = 7
+
+	const (
+		MONDAY       = 1
+		TUESDAY      = 2
+		WENSDAY      = 3
+		THURSDAY int = 4
+	)
+}
